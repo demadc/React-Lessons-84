52682826b6c07f8c5083a256104240a5251e97e7
Demian <demyan.korotchenko@gmail.com>
2024-01-20 10:06:23 -0800

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,1 +1,8 @@
 import axios from 'axios';
+
+axios.defaults.baseURL = 'https://64ecdd2ff9b2b70f2bfb120c.mockapi.io/api/v1';
+
+export const fetchQuizzes = async (req, res) => {
+  const response = await axios.get('/quizzes');
+  return res.data;
+};
